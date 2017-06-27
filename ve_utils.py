07e8733a9e55bfc592878a1a5487496d67332a32
Izak Burger <isburger@gmail.com>
2017-06-27 08:25:07 +0000

    dbusmonitor: Make dbus-unwrapping of integers faster.
     - re-arrange the block of type tests so we test for integers first,
       because the bulk of our data is of this kind. Less time wasted
       testing for the wrong type.
    
     - Put the list of dbus integer types we test for in a global variable
       so we only construct it once. Otherwise we construct that tuple on
       every signal we receive from dbus. This saves a miniscule 25
       microseconds, but it all adds up.

--- ve_utils.py
+++ ve_utils.py
@@ -144,13 +144,15 @@ def wrap_dbus_value(value):
 	return value
 
 
+dbus_int_types = (dbus.Int32, dbus.UInt32, dbus.Byte, dbus.Int16, dbus.UInt16, dbus.UInt32, dbus.Int64, dbus.UInt64)
+
 def unwrap_dbus_value(val):
 	"""Converts D-Bus values back to the original type. For example if val is of type DBus.Double,
 	a float will be returned."""
+	if isinstance(val, dbus_int_types):
+		return int(val)
 	if isinstance(val, dbus.Double):
 		return float(val)
-	if isinstance(val, (dbus.Byte, dbus.Int16, dbus.UInt16, dbus.Int32, dbus.UInt32, dbus.Int64, dbus.UInt64)):
-		return int(val)
 	if isinstance(val, dbus.Array):
 		v = [unwrap_dbus_value(x) for x in val]
 		return None if len(v) == 0 else v
